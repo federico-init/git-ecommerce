5bde381c991688fdfb84ac4bf2936b779f0f387f
unknown <fefe.borrometi@gmail.com>
2023-06-28 16:54:07 +0200

    feat: navbar first draft

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@
 import "./App.css";
 
 function App() {
-  return;
+  return <Navbar />;
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+// import css
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <a href="#">About Us</a>
+        <a href="#">Collection</a>
+        <a href="#">Contacts</a>
+      </div>
+      <div>
+        <p>Search</p>
+        <p>Cart</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
